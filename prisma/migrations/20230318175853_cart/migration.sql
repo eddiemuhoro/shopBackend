906d93877bcf879b7b1ec9913b2ecf974640fed4
eddiemuhoro <eddiemuhoro@gmail.com>
2023-03-19 17:07:04 +0300

    migrations

---
+++ prisma/migrations/20230318175853_cart/migration.sql
@@ -0,0 +1,5 @@
+-- DropForeignKey
+ALTER TABLE "orders" DROP CONSTRAINT "orders_productId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "orders" ADD CONSTRAINT "orders_productId_fkey" FOREIGN KEY ("productId") REFERENCES "product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
